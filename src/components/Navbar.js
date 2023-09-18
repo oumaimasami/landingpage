ed2d37f1d6a51784cbd14eacb1725c22950bf3b2
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2023-09-18 18:01:32 +0100

    <<<

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="../App.js">
           Navbar
         </a>
         <button
@@ -20,17 +20,14 @@ export default function Navbar() {
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">
+            <a class="nav-link active" aria-current="page" href="../App.js">
               Home
             </a>
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="../App.js">
               Features
             </a>
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="../App.js">
               Pricing
-            </a>
-            <a class="nav-link disabled" aria-disabled="true">
-              Disabled
             </a>
           </div>
         </div>
