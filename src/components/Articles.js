5f05ffa6bfd0df343269cbdd6754ff435096f599
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2023-10-01 20:42:01 +0100

    <<<

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -3,55 +3,52 @@ import "./articles.css";
 
 export default function Articles() {
   return (
-    <div className="row justify-content-center articles pt-4 mt-4 ">
-      <div className="col-10">
-        <div class="input-group input-group-lg">
-          <input
-            type="text"
-            className="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
+    <div className="row articles justify-content-center pt-4 mt-4 ">
+      <div className="input-group input-group-lg search">
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        <button type="button" className="btn btn-info">
+          Search
+        </button>
       </div>
-      <button type="button" className="btn btn-info">
-        Search
-      </button>
-      <div className="col-10">
-        <div className="form-check form-check-inline">
+      <div className="col-10 row py-4">
+        <div className=" py-2 mx-2 check">
+          <label className="form-check-label px-2" for="inlineCheckbox1">
+            Unisex
+          </label>
           <input
             className="form-check-input"
             type="checkbox"
             id="inlineCheckbox1"
             value="option1"
           />
-          <label className="form-check-label" for="inlineCheckbox1">
-            For Kids
-          </label>
         </div>
-        <div className="col-10">
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="inlineCheckbox2"
-              value="option2"
-            />
-            <label className="form-check-label" for="inlineCheckbox2">
-              For Females
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="inlineCheckbox3"
-              value="option3"
-            />
-            <label className="form-check-label" for="inlineCheckbox3">
-              For Man
-            </label>
-          </div>
+
+        <div className="py-2 mx-2 check">
+          <label className="form-check-label px-2" for="inlineCheckbox2">
+            For Her
+          </label>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox2"
+            value="option2"
+          />
+        </div>
+        <div className="py-2 mx-2 check">
+          <label className="form-check-label px-2" for="inlineCheckbox3">
+            For Him
+          </label>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox3"
+            value="option3"
+          />
         </div>
       </div>
     </div>
